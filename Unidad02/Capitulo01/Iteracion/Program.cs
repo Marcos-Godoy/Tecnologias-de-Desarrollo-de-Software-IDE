21b938ded65afa3baaf3858a3e154ede285c5469
Marcos-Godoy <mjgodoy2002@gmail.com>
2024-05-11 20:47:55 -0300

    agrego unidad 2 cap 1

---
+++ Unidad02/Capitulo01/Iteracion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] arrayStrings = new string[cantIteraciones];
+
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine($"Ingrese el elemento {i + 1}: ");
+                arrayStrings[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Elementos ingresados en orden inverso:");
+            for (int i = cantIteraciones - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arrayStrings[i]);
+            }
+        }
+    }
+}
---
+++ Unidad02/Capitulo01/Lab01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputTexto = Console.ReadLine();
+            if(inputTexto != "")
+            {
+                Console.WriteLine("Se ingreso texto");
+            } else
+            {
+                Console.WriteLine("No se ingreso texto");
+            }
+
+            ConsoleKeyInfo opcion = Console.ReadKey();
+            Console.WriteLine("Ingrese una opcion...\n 1) Mayuscula\n 2) Minuscula\n 3) Longitud");
+            /*
+            // menu de opciones con if anidados
+            
+            if (opcion.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine(inputTexto.ToUpper());
+            } else if(opcion.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine(inputTexto.ToLower());
+            } else if( opcion.Key == ConsoleKey.D3)
+            {
+                Console.WriteLine(inputTexto.Length);
+            } else
+            {
+                Console.WriteLine("Ingreso incorrecto...");
+            }
+            */
+
+            // menu de opciones con case
+            switch(opcion.Key) {
+                case ConsoleKey.D1:
+                    Console.WriteLine(inputTexto.ToUpper());
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine(inputTexto.ToLower());
+                    break;
+                case ConsoleKey.D3:
+                    Console.WriteLine(inputTexto.Length);
+                    break;
+                default:
+                    Console.WriteLine("Ingreso incorrecto...");
+                    break;
+            }
+        }
+    }
+}

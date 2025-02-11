8f16138b8bb4a316aacbbc3e6098751c404d0cf3
Marcos-Godoy <mjgodoy2002@gmail.com>
2025-02-11 09:17:19 -0300

    Borro algunos proyectos grandes

--- Proyectos/Inmobiliaria-ResolucionParcial - v1.0.0/Inmobiliaria-ResolucionParcial/Inmobiliaria.Domain/TipoPropiedad.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Inmobiliaria.Domain
-{
-    public class TipoPropiedad
-    {
-        public int Id { get; set; }
-        public string Descripcion { get; set; }       
-    }
-}
--- Proyectos/Inmobiliaria-ResolucionParcial - v1.0.0/Inmobiliaria-ResolucionParcial/Inmobiliaria.UI.Desktop/ApiClients/TipoPropiedadDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Inmobiliaria.UI.Desktop.ApiCliients
-{
-    public class TipoPropiedadDto
-    {
-        public int Id { get; set; }
-        public string Descripcion { get; set; }
-    }
-}
